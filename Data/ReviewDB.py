2b8e52d9ca296bcfcde957eb79664fff228ee051
yinonh <52419140+yinonh@users.noreply.github.com>
2023-06-22 14:45:24 +0300

    scarping for all the review data is completed

--- Data/ReviewDB.py
+++ Data/ReviewDB.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 import csv
+import os
 import datetime
 import random
 
@@ -17,13 +18,84 @@ class ReviewDB(Data):
         self.client_name = "ReviewDB"
         self.db_url = 'mongodb://localhost:27017'
 
-    def EVR_data(self):
+    # def EVR_data(self):
+    #
+    #     # Launch a new Chrome browser
+    #     driver = webdriver.Chrome()
+    #
+    #     # Navigate to the page with the links
+    #     driver.get()
+    #
+    #     # Find all <a> elements on the page
+    #     links = driver.find_elements(By.TAG_NAME, 'a')
+    #
+    #     # Define a regular expression to match the desired format
+    #     pattern = re.compile(r'https://www\.thestudent\.co\.il/Degrees/Degree_\d+\.html')
+    #
+    #     # Loop through each link and click on it if it matches the desired format
+    #     desired_link = ''
+    #     for link in links:
+    #         href = link.get_attribute('href')
+    #         if href and 'school93-d:0-dt:1' in href:
+    #             desired_link = href
+    #
+    #     driver.get(desired_link)
+    #     rElement = driver.find_element(By.CSS_SELECTOR,
+    #         ".CatRight")
+    #     lElement = driver.find_element(By.CSS_SELECTOR,
+    #         ".CatLeft")  # Replace with the appropriate selector for your lElement
+    #
+    #     # Get the HTML content of the lElement
+    #     lHtml_content = lElement.get_attribute("innerHTML")
+    #     rHtml_content = rElement.get_attribute("innerHTML")
+    #
+    #     rSoup = BeautifulSoup(rHtml_content, 'html.parser')
+    #     lSoup = BeautifulSoup(lHtml_content, 'html.parser')
+    #
+    #     # Extract the desired values
+    #     data = {}
+    #
+    #     # Extract numeric values using regular expressions
+    #     rating_count = lSoup.find(itemprop="ratingCount").text
+    #     data['ביקורות של סטודנטים ובוגרים'] = int(rating_count)
+    #     data['מסלולי לימוד'] = int(lSoup.find(itemprop="ratingCount").find_next_sibling("span").text)
+    #
+    #     response_percentages = lSoup.find_all(class_="Data")
+    #     response_values = [re.search(r'\d+', resp.text).group() for resp in response_percentages]
+    #     response_labels = ['ענה בצורה מלאה', 'ענה בצורה חלקית', 'לא ענה על הציפיות', 'לא ממליץ לאחרים']
+    #     data.update(dict(zip(response_labels, map(int, response_values))))
+    #
+    #     employment_percentage = lSoup.find(class_="BarOut").div.text.strip('%')
+    #     data['הבוגרים המועסקים בתחומם'] = int(employment_percentage)
+    #
+    #     study_level = lSoup.find('span', style="direction:rtl").text.strip('()')
+    #     data['רמת הלימודים'] = study_level
+    #
+    #     overall_rating = lSoup.find(itemprop="ratingValue").text
+    #     data['דירוג כללי'] = float(overall_rating)
+    #
+    #     # Print the dictionary
+    #     print(data)
+    #
+    #     links = rSoup.select('div.Middle')
+    #     links_dict = {}
+    #
+    #     for link in links:
+    #         title_element = link.select_one('div.Title a')
+    #         if title_element is not None:
+    #             title = title_element.text[6:-11].strip()
+    #             href = title_element.get('href')
+    #             links_dict[title] = href
+    #
+    #     self.write_to_csv(links_dict, 'EVR')
+
+    def collect_data(self, url, school_num, institute):
 
         # Launch a new Chrome browser
         driver = webdriver.Chrome()
 
         # Navigate to the page with the links
-        driver.get('https://www.thestudent.co.il/school93_%d7%94%d7%90%d7%95%d7%a0%d7%99%d7%91%d7%a8%d7%a1%d7%99%d7%98%d7%94_%d7%94%d7%a2%d7%91%d7%a8%d7%99%d7%aa')
+        driver.get(url)
 
         # Find all <a> elements on the page
         links = driver.find_elements(By.TAG_NAME, 'a')
@@ -35,78 +107,7 @@ class ReviewDB(Data):
         desired_link = ''
         for link in links:
             href = link.get_attribute('href')
-            if href and 'school93-d:0-dt:1' in href:
-                desired_link = href
-
-        driver.get(desired_link)
-        rElement = driver.find_element(By.CSS_SELECTOR,
-            ".CatRight")
-        lElement = driver.find_element(By.CSS_SELECTOR,
-            ".CatLeft")  # Replace with the appropriate selector for your lElement
-
-        # Get the HTML content of the lElement
-        lHtml_content = lElement.get_attribute("innerHTML")
-        rHtml_content = rElement.get_attribute("innerHTML")
-
-        rSoup = BeautifulSoup(rHtml_content, 'html.parser')
-        lSoup = BeautifulSoup(lHtml_content, 'html.parser')
-
-        # Extract the desired values
-        data = {}
-
-        # Extract numeric values using regular expressions
-        rating_count = lSoup.find(itemprop="ratingCount").text
-        data['ביקורות של סטודנטים ובוגרים'] = int(rating_count)
-        data['מסלולי לימוד'] = int(lSoup.find(itemprop="ratingCount").find_next_sibling("span").text)
-
-        response_percentages = lSoup.find_all(class_="Data")
-        response_values = [re.search(r'\d+', resp.text).group() for resp in response_percentages]
-        response_labels = ['ענה בצורה מלאה', 'ענה בצורה חלקית', 'לא ענה על הציפיות', 'לא ממליץ לאחרים']
-        data.update(dict(zip(response_labels, map(int, response_values))))
-
-        employment_percentage = lSoup.find(class_="BarOut").div.text.strip('%')
-        data['הבוגרים המועסקים בתחומם'] = int(employment_percentage)
-
-        study_level = lSoup.find('span', style="direction:rtl").text.strip('()')
-        data['רמת הלימודים'] = study_level
-
-        overall_rating = lSoup.find(itemprop="ratingValue").text
-        data['דירוג כללי'] = float(overall_rating)
-
-        # Print the dictionary
-        print(data)
-
-        links = rSoup.select('div.Middle')
-        result = {}
-
-        for link in links:
-            title_element = link.select_one('div.Title a')
-            if title_element is not None:
-                title = title_element.text[6:-11].strip()
-                href = title_element.get('href')
-                result[title] = href
-
-        print(result)
-
-    def BGU_data(self):
-
-        # Launch a new Chrome browser
-        driver = webdriver.Chrome()
-
-        # Navigate to the page with the links
-        driver.get('https://www.thestudent.co.il/school91_%d7%90%d7%95%d7%a0%d7%99%d7%91%d7%a8%d7%a1%d7%99%d7%98%d7%aa_%d7%91%d7%9f_%d7%92%d7%95%d7%a8%d7%99%d7%95%d7%9f')
-
-        # Find all <a> elements on the page
-        links = driver.find_elements(By.TAG_NAME, 'a')
-
-        # Define a regular expression to match the desired format
-        pattern = re.compile(r'https://www\.thestudent\.co\.il/Degrees/Degree_\d+\.html')
-
-        # Loop through each link and click on it if it matches the desired format
-        desired_link = ''
-        for link in links:
-            href = link.get_attribute('href')
-            if href and 'school91-d:0-dt:1' in href:
+            if href and f'{school_num}-d:0-dt:1' in href:
                 desired_link = href
 
         driver.get(desired_link)
@@ -149,7 +150,6 @@ class ReviewDB(Data):
 
         links = rSoup.select('div.Middle')
         links_dict = {}
-
         for link in links:
             title_element = link.select_one('div.Title a')
             if title_element is not None:
@@ -157,19 +157,72 @@ class ReviewDB(Data):
                 href = title_element.get('href')
                 links_dict[title] = href
 
-        for title, href in links_dict.items():
-            url = "https://www.thestudent.co.il" + href
-            response = requests.get(url)
+        self.write_to_csv(links_dict, institute)
 
-            if response.status_code == 200:
-                html = response.text
-                # Process the HTML as per your requirements
-                print(f"HTML for '{title}': {html}")
-            else:
-                print(f"Failed to retrieve HTML for '{title}'")
+    def write_to_csv(self, links_dict, institute):
+        file_exists = os.path.isfile('reviews.csv')
+
+        with open('reviews.csv', 'a', newline='', encoding='utf-8-sig') as csvfile:
+            fieldnames = ['institute', 'field', 'why', 'expectations', 'level', 'advice', 'rating_value']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if not file_exists:
+                writer.writeheader()
+
+            for title, href in links_dict.items():
+                url = "https://www.thestudent.co.il" + href
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    html = response.text
+                    soup = BeautifulSoup(html, 'html.parser')
+
+                    institute = institute
+                    field = title
+
+                    reviews = soup.select('.Review')
+                    for review in reviews:
+                        content = review.select('.Info .M-OUT')
+                        why = content[0].text
+                        expectations = content[1].text
+                        level = content[2].text
+                        advice = content[3].text
+
+                        try:
+                            rating_element = review.select_one('.Grades div[itemprop="ratingValue"]')
+                            rate = rating_element.text.strip()
+                        except AttributeError:
+                            rate = ""
+
+                        writer.writerow({
+                            'institute': institute,
+                            'field': field,
+                            'why': why,
+                            'expectations': expectations,
+                            'level': level,
+                            'advice': advice,
+                            'rating_value': rate
+                        })
+
+                    print(f"All reviews for '{title}' have been written to the CSV file.")
+                else:
+                    print(f"Failed to retrieve data for '{title}'.")
 
     def update_all(self):
-        self.BGU_data()
+        if os.path.exists('reviews.csv'):
+            os.remove('reviews.csv')
+
+        self.collect_data('https://www.thestudent.co.il/school91_%d7%90%d7%95%d7%a0%d7%99%d7%91%d7%a8%d7%a1%d7%99%d7%98%d7%aa_%d7%91%d7%9f_%d7%92%d7%95%d7%a8%d7%99%d7%95%d7%9f',
+                          "school91", 'BGU')  # BGU
+        self.collect_data(
+            'https://www.thestudent.co.il/school93_%d7%94%d7%90%d7%95%d7%a0%d7%99%d7%91%d7%a8%d7%a1%d7%99%d7%98%d7%94_%d7%94%d7%a2%d7%91%d7%a8%d7%99%d7%aa',
+                          "school93", 'EVR')  # EVR
+        self.collect_data(
+            'https://www.thestudent.co.il/school90_%d7%90%d7%95%d7%a0%d7%99%d7%91%d7%a8%d7%a1%d7%99%d7%98%d7%aa_%d7%aa%d7%9c_%d7%90%d7%91%d7%99%d7%91',
+            "school90", 'TLV')  # TLV
+        self.collect_data(
+            'https://www.thestudent.co.il/school94_%d7%94%d7%98%d7%9b%d7%a0%d7%99%d7%95%d7%9f',
+            "school94", 'TECH')  # TECH
 
 x = ReviewDB()
 x.update_all()
